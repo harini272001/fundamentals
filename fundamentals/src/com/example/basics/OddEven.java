3aef79eea7d017f60750c27cd6b0c3d973eacca5
Harini <Harini@LAPTOP-J8E2971H>
2026-08-11 19:44:50 +0530

    Added java basic programs

---
+++ fundamentals/src/com/example/basics/OddEven.java
@@ -0,0 +1,15 @@
+package com.example.basics;
+
+public class OddEven {
+	public static void main(String[] args) {
+		int num = 43;
+		if(num%2==0) 
+		{
+			System.out.println(num + " is even number");
+		}
+		else 
+		{
+			System.out.println(num + " is odd number");
+		}
+	}
+}
