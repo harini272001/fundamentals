a4381dbafa0cb5251b030d58fb8c54fb210de45b
Harini <Harini@LAPTOP-J8E2971H>
2026-08-11 21:53:57 +0530

    Added factorial program and multiplication program

---
+++ fundamentals/src/com/example/basics/Factorial.java
@@ -0,0 +1,17 @@
+package com.example.basics;
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int factNum = sc.nextInt();
+		int num = 1;
+		for (int i = 1; i <= factNum; i++ )
+		{
+			num = num * i;
+		}
+		System.out.println(num);
+	}
+}
---
+++ fundamentals/src/com/example/basics/Multiplication.java
@@ -0,0 +1,16 @@
+package com.example.basics;
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a numer: ");
+		int a = sc.nextInt();
+		for (int i = 1; i<= 10; i++) 
+		{
+			int b = i * a;
+			System.out.println(i + " * " + a + " = " + b);
+		}
+	}
+}
